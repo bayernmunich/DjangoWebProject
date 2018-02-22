15ace83b16a239af1b566608b0257961a5373331
bayernmunich <aniruddhajere@outlook.com>
2018-02-22 16:12:56 +0530

    Add project files.

---
+++ DjangoWebProject2/env_my/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\aniruddhaj\source\repos\DjangoWebProject2\DjangoWebProject2\env_my\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
